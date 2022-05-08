2ca328ae0c1150d3ec26924e2d83c607be9a06fc
Ariel-mcu <judy3113461@gmail.com>
2022-05-08 14:55:23 +0800

    更新sum.js

--- basic/sum.js
+++ basic/sum.js
@@ -1,3 +1,9 @@
-function sun(n) {
-    //return 結果
+// 回傳 1 + 2 + ... + n 的結果 
+function sum(n) {
+    let sum =0;
+    for (let i =0; i <= n; i ++){
+        sum += i;
+    }
+    return sum;
 }
+console.log(sum(2));
