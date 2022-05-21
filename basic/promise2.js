ccfef51c8856909b966f1434b37fb8f1ba3f68ca
Ariel-mcu <judy3113461@gmail.com>
2022-05-21 15:45:39 +0800

    add promise2

---
+++ basic/promise2.js
@@ -0,0 +1,19 @@
+const fs = require('fs/promises');
+
+async function test() {
+  console.log(2);
+  let r = await fs.readFile('stock.txt', 'utf-8');
+  console.log(r);
+  return r;
+}
+
+// (async () => {
+//     console.log(1);
+//         let result =await test();
+// console.log(3, result);
+// })();
+
+console.log(1);
+let result = test();
+console.log(3, result); // promise<pending>
+
