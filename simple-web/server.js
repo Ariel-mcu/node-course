cec0c12cd763fcb4115cd73c9201f3080e917816
Ariel-mcu <judy3113461@gmail.com>
2022-05-28 13:48:15 +0800

    add simple-example & dir

--- simple-web/server.js
+++ simple-web/server.js
@@ -1,7 +1,8 @@
+// http 是 NodeJS 內建的模組
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-
+    // 當你的 server 接收到 request 的時候要做什麼事
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/html;charest=UTF-8');
     response.end('hello server');
